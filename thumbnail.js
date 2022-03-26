c2eda3f63cbcdd734495144caad5c4e052827299
linyujun123 <39055435+linyujun123@users.noreply.github.com>
2022-03-27 01:58:23 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-   targetImage.addEventListener("click", function() {
-    alert("点击了图片");
+   targetImage.classList.add("small");
+targetImage.classList.remove("small");
 });
 });
